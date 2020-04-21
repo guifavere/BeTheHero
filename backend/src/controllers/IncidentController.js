d75906d46605b1352ac49e971523b2c576a25160
Guilherme de Favere <guilherme.favere@gmail.com>
2020-04-21 15:45:33 -0300

    Fix incident deleting

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -32,7 +32,7 @@ module.exports = {
 
     const incident = await connection('incidents').where('id', id).select('ong_id').first();
 
-    if (incident.ong_id !== ong_id) {
+    if (!incident || incident.ong_id !== ong_id) {
       return res.status(401).json({ error: 'Operation not permitted.' });
     }
 
